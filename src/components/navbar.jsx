159f02b0bc324c7802273dbbedcaf5d5daedeb0c
Fernando Furtado <fernando.furtadocarrilho@gmail.com>
2022-12-09 23:59:44 -0300

    search

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
+
 import AppBar from '@mui/material/AppBar';
+import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
@@ -13,8 +15,12 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import Slide from '@mui/material/Slide';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
-import './navbar.css'
 import { Link } from 'react-router-dom';
+
+import './navbar.css';
+import Search from '../components/search';
+import SlideHorizontal from "../components/SlideHorizontal";
+import movies from '../assets/json/movies.json';
 
 const pages = ['Inicio', 'Lançamentos', 'Favoritos'];
 const settings = ['Conta', 'Idioma', 'Sair'];
@@ -50,6 +56,25 @@ function NavBar(props) {
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
+  };
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: '#202023d9',
+    border: '2px solid #ffffff92',
+    borderRadius: 2,
+    boxShadow: 24,
+    p: 4,
+    width: '70%',
+    height: '60%',
   };
 
   return (
@@ -106,17 +131,17 @@ function NavBar(props) {
                     display: { xs: 'block', md: 'none' },
                   }}
                 >
-                    <MenuItem key={'Inicio'} href="/" onClick={handleCloseNavMenu}>
-                      <Typography textAlign="center">Inicio</Typography>
-                    </MenuItem>
+                  <MenuItem key={'Inicio'} href="/" onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">Inicio</Typography>
+                  </MenuItem>
 
-                    <MenuItem key={'Lancamentos'} onClick={handleCloseNavMenu}>
-                      <Typography textAlign="center">Lançamentos</Typography>
-                    </MenuItem>
+                  <MenuItem key={'Lancamentos'} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">Lançamentos</Typography>
+                  </MenuItem>
 
-                    <MenuItem key={'Favoritos'} onClick={handleCloseNavMenu}>
-                      <Typography textAlign="center">Favoritos</Typography>
-                    </MenuItem>
+                  <MenuItem key={'Favoritos'} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">Favoritos</Typography>
+                  </MenuItem>
 
                 </Menu>
               </Box>
@@ -139,35 +164,57 @@ function NavBar(props) {
               >
                 BRFLIX
               </Typography>
-              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-              <Button
-                    key={'Inicio'}
-                    onClick={handleCloseNavMenu}
-                    sx={{ my: 2, color: 'white', display: 'block' }}
-                    href="/"
-                    className="hover-primary"
-                  >
-                    Inicio
-                  </Button>
+              <Button onClick={handleOpen}>
+                <Search info={'Search...'} />
+              </Button>
 
-                  <Button
-                    key={'Lancamentos'}
-                    onClick={handleCloseNavMenu}
-                    sx={{ my: 2, color: 'white', display: 'block' }}
-                    href="#"
-                    className="hover-primary"
-                  >
-                    Lançamentos
-                  </Button>
-                  <Button
-                    key={'Favoritos'}
-                    onClick={handleCloseNavMenu}
-                    sx={{ my: 2, color: 'white', display: 'block' }}
-                    href="#"
-                    className="hover-primary"
-                  >
-                    Favoritos
-                  </Button>
+              <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                  <Typography id="modal-modal-title " variant="h6" component="h2">
+                    <Search info={'Digite aqui...'} />
+                  </Typography>
+                  <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    <Box className="row mt-3">
+                      <SlideHorizontal {...{ data: movies, height: 200, label: "fs-4 text" }} />
+                    </Box>
+                  </Typography>
+                </Box>
+                {/* <Button onClick={handleClose}>Close Child Modal</Button> */}
+              </Modal>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                <Button
+                  key={'Inicio'}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                  href="/"
+                  className="hover-primary"
+                >
+                  Inicio
+                </Button>
+
+                <Button
+                  key={'Lancamentos'}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                  href="#"
+                  className="hover-primary"
+                >
+                  Lançamentos
+                </Button>
+                <Button
+                  key={'Favoritos'}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                  href="#"
+                  className="hover-primary"
+                >
+                  Favoritos
+                </Button>
               </Box>
               <Box sx={{ flexGrow: 0 }}>
                 <Tooltip title="Abrir configurações">
@@ -196,11 +243,11 @@ function NavBar(props) {
                 >
                   {settings.map((setting) => (
                     <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                        <Link 
-                          to={setting === 'Conta' ? `/account`: '#'}
-                          className="text-decoration-none text-light">
-                          {setting}
-                        </Link>
+                      <Link
+                        to={setting === 'Conta' ? `/account` : '#'}
+                        className="text-decoration-none text-light">
+                        {setting}
+                      </Link>
                     </MenuItem>
                   ))}
                 </Menu>
